b86db054bcae87c3a06def08e1c8c315145fc4a9
RalphAdi <153388309+RalphAdi@users.noreply.github.com>
2024-02-10 13:31:06 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,30 +9,44 @@ const Composite = Matter.Composite;
 
 let engine;
 let world;
-var ground;
-var fruit,rope;
+var rope,fruit,ground;
 var fruit_con;
 
 var bg_img;
 var food;
-var rabbit;
+var bunny_img;
+
+var button;
+var bunny;
 
 function preload()
 {
   bg_img = loadImage('background.png');
   food = loadImage('melon.png');
-  rabbit = loadImage('Rabbit-01.png');
+  bunny_img = loadImage('Rabbit-01.png');
 }
 
 function setup() 
 {
   createCanvas(500,700);
   frameRate(80);
+
   engine = Engine.create();
   world = engine.world;
-  ground = new Ground(200,680,600,20);
 
-  rope = new Rope(7,{x:245,y:30});
+  //btn 1
+  button = createImg('cut_btn.png');
+  button.position(200,30);
+  button.size(50,50);
+  button.mouseClicked(drop);
+
+  
+  rope = new Rope(8,{x:220,y:30});
+  ground = new Ground(200,690,600,20);
+  bunny = createSprite(200,620,100,100);
+  bunny.addImage(bunny_img);
+  bunny.scale = 0.2;
+
   fruit = Bodies.circle(300,300,20);
   Matter.Composite.add(rope.body,fruit);
 
@@ -41,21 +55,32 @@ function setup()
   rectMode(CENTER);
   ellipseMode(RADIUS);
   textSize(50)
-  imageMode(CENTER);
   
 }
 
 function draw() 
 {
   background(51);
+  image(bg_img,0,0,displayWidth+80,displayHeight);
 
-  image(bg_img,width/2,height/2,490,690);
+  push();
+  imageMode(CENTER);
+  if(fruit!=null){
+    image(food,fruit.position.x,fruit.position.y,70,70);
+  }
+  pop();
 
-  image(food,fruit.position.x,fruit.position.y,70,70);
   rope.show();
+
   Engine.update(engine);
   ground.show();
-
- 
+  drawSprites();
    
+}
+
+function drop()
+{
+  rope.break();
+  fruit_con.detach();
+  fruit_con = null; 
 }
